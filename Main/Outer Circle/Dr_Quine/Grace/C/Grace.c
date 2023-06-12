a88e4b3c5cb46e53a450d9ae0a8f375f662a4afa
fallen8 <sorksoek2@gmail.com>
2023-06-12 14:57:52 +0900

    IMPLEMENTED C part

--- Main/Outer Circle/Dr_Quine/Grace/C/Grace.c
+++ Main/Outer Circle/Dr_Quine/Grace/C/Grace.c
@@ -4,7 +4,7 @@
 /*
 	This program will print its own source when run.
 */
-#define S "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c/*%c%cThis program will print its own source when run.%c*/%c#define S %c%s%c%c#define FD open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC)%c#define FT()int main(){ int fd = FD; dup2(fd, STDOUT_FILENO); close(fd); printf(S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 10, 10); return 0; }%cFT();"
+#define S "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c/*%c%cThis program will print its own source when run.%c*/%c#define S %c%s%c%c#define FD open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC)%c#define FT()int main(){ int fd = FD; dup2(fd, STDOUT_FILENO); close(fd); printf(S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 10, 10); fflush(stdout); close(STDOUT_FILENO); return 0; }%cFT();"
 #define FD open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC)
-#define FT()int main(){ int fd = FD; dup2(fd, STDOUT_FILENO); close(fd); printf(S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 10, 10); printf("\n"); close(STDOUT_FILENO); return 0; }
+#define FT()int main(){ int fd = FD; dup2(fd, STDOUT_FILENO); close(fd); printf(S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 10, 10); fflush(stdout); close(STDOUT_FILENO); return 0; }
 FT();
--- Main/Outer Circle/Dr_Quine/Sully/C/Sully.c
+++ Main/Outer Circle/Dr_Quine/Sully/C/Sully.c
@@ -3,10 +3,9 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-#define CODE "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <string.h>%c#define CODE %c%s%c%c#define SOURCE %cSully_%d.c%c%c#define EXECUTE %cSully_%d%c%c#define I %d%c#define FILE fopen(SOURCE, %cw%c)%c#define FT()int main(){ fprintf(FILE, CODE, 10, 10, 10, 10, 10, 34, CODE, 34, 10, 34, I - 1, 34, 10, 34, I - 1, 34, 10, I - 1, 10, 34, 34, 10, 34, 37, 37, 34, 34, 37, 34, 10); fclose(FILE); if (!I) { return 0; } char compile[50]; char cmd[50]; bzero(compile, 50); bzero(cmd, 50); sprintf(compile, %ccc -Wall -Wextra -Werror %cs -o %cs%c, SOURCE, EXECUTE); sprintf(cmd, %c./%cs%c, EXECUTE); system(compile); system(cmd); return 0; }%cFT();"
+#define CODE "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <string.h>%c#define CODE %c%s%c%c#define SOURCE %cSully_%d.c%c%c#define EXECUTE %cSully_%d%c%c#define I %d%c#define FT()int main(){ FILE* file = fopen(SOURCE, %cw%c); fprintf(file, CODE, 10, 10, 10, 10, 10, 34, CODE, 34, 10, 34, I - 1, 34, 10, 34, I - 1, 34 , 10, I - 1, 10, 34, 34, 34, 37, 37, 34, 34, 37, 34, 10); fclose(file); if(!I) return 0; char compile[50]; bzero(compile, 50); sprintf(compile, %ccc -Wall -Werror -Wextra %cs -o %cs%c, SOURCE, EXECUTE); system(compile); char cmd[50]; bzero(cmd, 50); sprintf(cmd, %c./%cs%c, EXECUTE); system(cmd); return 0; }%cFT();"
 #define SOURCE "Sully_5.c"
 #define EXECUTE "Sully_5"
 #define I 5
-#define FILE fopen(SOURCE, "w")
-#define FT()int main(){ fprintf(FILE, CODE, 10, 10, 10, 10, 10, 34, CODE, 34, 10, 34, I - 1, 34, 10, 34, I - 1, 34, 10, I - 1, 10, 34, 34, 10, 34, 37, 37, 34, 34, 37, 34, 10); fclose(FILE); if (!I) { return 0; } char compile[50]; char cmd[50]; bzero(compile, 50); bzero(cmd, 50); sprintf(compile, "gcc -Wall -Wextra -Werror %s -o %s", SOURCE, EXECUTE); sprintf(cmd, "./%s", EXECUTE); system(compile); system(cmd); return 0; }
+#define FT()int main(){ FILE* file = fopen(SOURCE, "w"); fprintf(file, CODE, 10, 10, 10, 10, 10, 34, CODE, 34, 10, 34, I - 1, 34, 10, 34, I - 1, 34 , 10, I - 1, 10, 34, 34, 34, 37, 37, 34, 34, 37, 34, 10); fclose(file); if(!I) return 0; char compile[50]; bzero(compile, 50); sprintf(compile, "cc -Wall -Werror -Wextra %s -o %s", SOURCE, EXECUTE); system(compile); char cmd[50]; bzero(cmd, 50); sprintf(cmd, "./%s", EXECUTE); system(cmd); return 0; }
 FT();
