6f1446c66c1fce0f947ae365e53dbd5b6b3e08dd
fallen8 <sorksoek2@gmail.com>
2023-06-12 13:50:49 +0900

    WIP

--- Main/Outer Circle/Dr_Quine/Sully/C/Sully.c
+++ Main/Outer Circle/Dr_Quine/Sully/C/Sully.c
@@ -3,27 +3,10 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-
-extern char **environ;
-#define S "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <string.h>%c%cextern char **environ;%c#define S %c%s%c%cint main() {%c%cint i = %d;%c%cif (!i) {%c%c%creturn 0;%c%c}%c%cchar source[30], execute[30], compile[100];%c%cbzero(source, 30);%c%cbzero(execute, 30);%c%cbzero(compile, 100);%c%csprintf(source, %cSully_%cd.c%c, i);%c%csprintf(execute, %cSully_%cd%c, i);%c%cint fd = open(source, O_RDWR | O_CREAT | O_TRUNC);%c%cdup2(fd, 1);%c%cclose(fd);%c%c--i;%c%cprintf(S, 10, 10, 10, 10, 10, 10, 10, 34, S, 34, 10, 10, 9, i, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, 37, 34, 10, 9, 34, 37, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, 37, 37, 34, 10, 9, 10, 9, 10, 9, 10);%c%csprintf(compile, %ccc -Wall -Wextra -Werror %cs -o %cs%c, source, execute);%c%csystem(compile);%c%csystem(execute);%c%creturn 0;%c}"
-int main() {
-	int i = 5;
-	if (!i) {
-		return 0;
-	}
-	char source[30], execute[30], compile[100];
-	bzero(source, 30);
-	bzero(execute, 30);
-	bzero(compile, 100);
-	sprintf(source, "Sully_%d.c", i);
-	sprintf(execute, "Sully_%d", i);
-	int fd = open(source, O_RDWR | O_CREAT | O_TRUNC);
-	dup2(fd, STDOUT_FILENO);
-	close(fd);
-	--i;
-	// printf(S, 10, 10, 10, 10, 10, 10, 10, 34, S, 34, 10, 10, 9, i, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, 37, 34, 10, 9, 34, 37, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, 37, 37, 34, 10, 9, 10, 9, 10, 9, 10);
-	// sprintf(compile, "cc -Wall -Wextra -Werror %s -o %s", source, execute);
-	// system(compile);
-	// system(execute);
-	return 0;
-}
+#define CODE "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <string.h>%c#define CODE %c%s%c%c#define SOURCE %cSully_%d.c%c%c#define EXECUTE %cSully_%d%c%c#define I %d%c#define FILE fopen(SOURCE, %cw%c)%c#define FT()int main(){ fprintf(FILE, CODE, 10, 10, 10, 10, 10, 34, CODE, 34, 10, 34, I - 1, 34, 10, 34, I - 1, 34, 10, I - 1, 10, 34, 34, 10, 34, 37, 37, 34, 34, 37, 34, 10); fclose(FILE); if (!I) { return 0; } char compile[50]; char cmd[50]; bzero(compile, 50); bzero(cmd, 50); sprintf(compile, %ccc -Wall -Wextra -Werror %cs -o %cs%c, SOURCE, EXECUTE); sprintf(cmd, %c./%cs%c, EXECUTE); system(compile); system(cmd); return 0; }%cFT();"
+#define SOURCE "Sully_5.c"
+#define EXECUTE "Sully_5"
+#define I 5
+#define FILE fopen(SOURCE, "w")
+#define FT()int main(){ fprintf(FILE, CODE, 10, 10, 10, 10, 10, 34, CODE, 34, 10, 34, I - 1, 34, 10, 34, I - 1, 34, 10, I - 1, 10, 34, 34, 10, 34, 37, 37, 34, 34, 37, 34, 10); fclose(FILE); if (!I) { return 0; } char compile[50]; char cmd[50]; bzero(compile, 50); bzero(cmd, 50); sprintf(compile, "gcc -Wall -Wextra -Werror %s -o %s", SOURCE, EXECUTE); sprintf(cmd, "./%s", EXECUTE); system(compile); system(cmd); return 0; }
+FT();
