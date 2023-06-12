266a51353522a3716b2feb8124695456c0c406e0
fallen8 <sorksoek2@gmail.com>
2023-06-12 15:17:12 +0900

    FIX: Grace not use open but fopen

--- Main/Outer Circle/Dr_Quine/Grace/C/Grace.c
+++ Main/Outer Circle/Dr_Quine/Grace/C/Grace.c
@@ -4,7 +4,6 @@
 /*
 	This program will print its own source when run.
 */
-#define S "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c/*%c%cThis program will print its own source when run.%c*/%c#define S %c%s%c%c#define FD open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC)%c#define FT()int main(){ int fd = FD; dup2(fd, STDOUT_FILENO); close(fd); printf(S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 10, 10); fflush(stdout); close(STDOUT_FILENO); return 0; }%cFT();"
-#define FD open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC)
-#define FT()int main(){ int fd = FD; dup2(fd, STDOUT_FILENO); close(fd); printf(S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 10, 10); fflush(stdout); close(STDOUT_FILENO); return 0; }
+#define S "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c/*%c%cThis program will print its own source when run.%c*/%c#define S %c%s%c%c#define FT()int main(){ FILE* file = fopen(%cGrace_kid.c%c, %cw%c); fprintf(file, S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 34, 34, 10); fclose(file); return 0; }%cFT();"
+#define FT()int main(){ FILE* file = fopen("Grace_kid.c", "w"); fprintf(file, S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 34, 34, 10); fclose(file); return 0; }
 FT();
