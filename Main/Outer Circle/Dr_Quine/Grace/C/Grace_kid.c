e5423ccabba238b4d00f3a6994576ccaa2544850
fallen8 <sorksoek2@gmail.com>
2023-06-12 15:01:57 +0900

    WIP: makefile

---
+++ Main/Outer Circle/Dr_Quine/Grace/C/Grace_kid.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+/*
+	This program will print its own source when run.
+*/
+#define S "#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c/*%c%cThis program will print its own source when run.%c*/%c#define S %c%s%c%c#define FD open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC)%c#define FT()int main(){ int fd = FD; dup2(fd, STDOUT_FILENO); close(fd); printf(S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 10, 10); fflush(stdout); close(STDOUT_FILENO); return 0; }%cFT();"
+#define FD open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC)
+#define FT()int main(){ int fd = FD; dup2(fd, STDOUT_FILENO); close(fd); printf(S, 10, 10, 10, 10, 9, 10, 10, 34, S, 34, 10, 34, 34, 10, 10); fflush(stdout); close(STDOUT_FILENO); return 0; }
+FT();
