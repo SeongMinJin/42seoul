b6211641134039fc94cb7b685554629a47031068
fallen8 <sorksoek2@gmail.com>
2023-08-15 20:25:23 +0900

    update make file, init header file for all functions to use

---
+++ Main/Outer Circle/ft_ping/ft_ping.h
@@ -0,0 +1,12 @@
+#ifndef FT_PING_H
+#define FT_PING_H
+
+#include <sys/time.h>// ◦ gettimeofday
+#include <stdlib.h>// ◦ exit
+#include <arpa/inet.h>// ◦ inet_ntop / inet_pton / nthos / htons 
+#include <signal.h>// ◦ signal
+#include <unistd.h>// ◦ alarm / usleep / close / getpid / getuid
+#include <sys/socket.h> // ◦ socket / setsockopt / sendto / recvmsg / getaddrinfo / getnameinfo / freeaddrinfo / gai_strerror
+#include <stdio.h> // ◦ strerror
+
+#endif
---
+++ Main/Outer Circle/ft_ping/other.c
@@ -0,0 +1,8 @@
+#include "ft_ping.h"
+
+void print(void)
+{
+	printf("Hello World");
+	printf("Hello World");
+	printf("Hello World");
+}
